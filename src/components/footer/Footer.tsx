8939e53710b7306e0d4083a585226c539157321b
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-03-12 10:09:42 +0300

    feat: update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,12 +5,13 @@ import Avatar from '@material-ui/core/Avatar';
 import logo from '../../assets/img/logo-rsschool.png';
 import inna from '../../assets/img/inna.jpg';
 import nastya from '../../assets/img/nastya.jpg';
+import sergey from '../../assets/img/sergey.jpg';
 import kira from '../../assets/img/kira.jpg';
 import { useStyles } from './Footer.style';
+import Link from '@material-ui/core/Link';
 
 const Footer = () => {
   const classes = useStyles();
-
   return (
     <footer className={classes.footer}>
       <Toolbar className={classes.toolbar}>
@@ -21,10 +22,18 @@ const Footer = () => {
           @2021
         </Typography>
         <div className={classes.team}>
-          <Avatar alt='Inna' src={inna} className={classes.small} />
-          <Avatar alt='Anastasiya' src={nastya} className={classes.small} />
-          <Avatar alt='Sergey' className={[classes.circle, classes.small].join(' ')}>SN</Avatar>
-          <Avatar alt='Kirill' src={kira} className={classes.large} />
+          <Link href="https://github.com/inna-rekesh" target="_blank" rel="noopener noreferrer" >
+            <Avatar alt='Inna Rekesh' src={inna} className={classes.small} />
+          </Link>
+          <Link href="https://github.com/anasidorovich" target="_blank" rel="noopener noreferrer" color="inherit">
+            <Avatar alt='Anastasiya Sidarovich' src={nastya} className={classes.small} />
+          </Link>
+          <Link href="https://github.com/SergeyNaumenko" target="_blank" rel="noopener noreferrer" variant="body2">
+            <Avatar alt='Sergey Naumenko' src={sergey} className={classes.small} />
+          </Link>
+          <Link href="https://github.com/magerrrr" target="_blank" rel="noopener noreferrer" variant="body2">
+            <Avatar alt='Kirill Mager' src={kira} className={classes.large} />
+          </Link>
         </div>
       </Toolbar>
     </footer>

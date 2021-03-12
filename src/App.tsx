569ed671ce5df3fdae7e2156fd69eafbe25ea96f
Sergey Naumenko <sergeynau@gmail.com>
2021-03-12 23:49:15 +0300

    feat(map-component): initial map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
 import Button from '@material-ui/core/Button';
+import MapComponent from './components/map';
 
 const App: React.FC = () => {
+  const capitalPosition = { lat: 26.613209, lng: 55.674835 }
+  const countryCode = 'blr';
   return (
     <div>
       <h1>Hello, Our team will create best travel application ever!</h1>
       <Button variant="contained">material UI button</Button>
+      
     </div>
   );
 };
---
+++ src/components/map.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import FullscreenControl from 'react-leaflet-fullscreen';
+//import 'react-leaflet-fullscreen/dist/styles.css'
+import {
+  Map,
+  TileLayer,
+  CircleMarker,
+  Tooltip,
+  LayersControl,
+  LayerGroup,
+  Marker,
+  Popup,
+  ScaleControl,
+  Polygon,
+  GeoJSON
+} from 'react-leaflet';
+
+type MapProps = {
+  capitalPosition: {
+    lat: number,
+    lng: number,
+  },
+  countryCode: string,
+  
+}
+
+const MapComponent = (props: MapProps) => {
+  const { 
+    capitalPosition, 
+    countryCode = 'blr',
+  } = props;
+
+  const [isLoading , setIsLoading] = useState(true);
+  const [geoData, setGeoData] = useState<any>({type:'Feature',properties:{cca2:''},geometry:{type:'Polygon',coordinates:[[]]}});
+
+  async function fetchData() {
+    const response = await fetch(`../data/${countryCode}.geo.json`,{
+      method: 'get',
+      headers: {
+          'Accept': 'application/json, text/plain, */*' ,
+          'Content-Type': 'application/json'
+        }
+    });
+
+    response
+      .json()
+      .then(data => {
+        if (data.features) {
+          setGeoData(data.features[0]);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    if (!isLoading) {
+      setIsLoading(true);
+    }
+    fetchData();
+  }, [countryCode]);
+
+  return (
+    <Map id="mapid" center={capitalPosition} zoom={9} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
+      />
+      <Marker position={capitalPosition}/>
+
+      <ScaleControl></ScaleControl>
+      <FullscreenControl position="topleft" />
+
+      <GeoJSON data={geoData}></GeoJSON>
+
+    </Map>
+  )
+}
+
+export default MapComponent

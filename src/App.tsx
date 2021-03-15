d96ebc55a9563b925620a5e819dcdcbf7773abaa
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-03-15 11:53:34 +0300

    feat: create video component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { I18nextProvider } from 'react-i18next';
 import Button from '@material-ui/core/Button';
-import "./i18n/config";
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import i18n from './i18n/config';
+import Video from "./components/video";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +14,11 @@ const App: React.FC = () => {
         <h1>Hello, Our team will create best travel application ever!</h1>
         <Button variant="contained">material UI button</Button>
       </div>
+      <Toolbar>
+        <Container maxWidth='md'>
+          <Video embedId="L5JORXmV_A0" />
+        </Container>
+      </Toolbar>
     </I18nextProvider>
   );
 };

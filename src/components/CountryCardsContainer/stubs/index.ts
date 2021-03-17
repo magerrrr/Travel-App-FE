98bcad0b9186603682e02e909ed58f2712ae1ef2
Kirill Mager <kiril.maher@itechart-group.com>
2021-03-17 22:18:55 +0300

    feat: add additional field with preview image to stubs

--- src/components/CountryCardsContainer/stubs/index.ts
+++ src/components/CountryCardsContainer/stubs/index.ts
@@ -13,54 +13,67 @@ const country1 = {
   name: 'Spain',
   capital: 'Madrid',
   image: spain,
+  preview: 'https://ratatum.com/wp-content/uploads/2018/03/1i.jpg',
 };
 const country2 = {
   id: 2,
   name: 'Belarus',
   capital: 'Minsk',
   image: minsk,
+  preview:
+    'https://d39raawggeifpx.cloudfront.net/styles/16_9_desktop/s3/articleimages/bneGeneric_Belarus_minsk_cityscape_Cropped.jpg',
 };
 const country8 = {
   id: 8,
   name: 'France',
   capital: 'Paris',
   image: paris,
+  preview: 'https://opentheoryproject.files.wordpress.com/2014/04/paris.jpg',
 };
 const country4 = {
   id: 4,
   name: 'Russia',
   capital: 'Moscow',
   image: russia,
+  preview:
+    'https://brand-mag.com/wp-content/uploads/Reyting-gorodov-Rossii-po-urovnyu-zhizni-2017-..jpg',
 };
 const country9 = {
   id: 9,
   name: 'Italy',
   capital: 'Rome',
   image: italy,
+  preview:
+    'https://www.roadaffair.com/wp-content/uploads/2017/09/colosseum-rome-italy-shutterstock_433413835.jpg',
 };
 const country6 = {
   id: 6,
   name: 'Germany',
   capital: 'Berlin',
   image: germany,
+  preview: 'https://www.kurortbest.ru/assets/images/4/germaniyu.jpg',
 };
 const country7 = {
   id: 7,
   name: 'Sweden',
   capital: 'Stockholm',
   image: sweden,
+  preview:
+    'https://avatars.mds.yandex.net/get-zen_doc/53963/pub_5e74876ef2eeaa232df9a9aa_5e748c9a45ca804afa832ad2/scale_1200',
 };
 const country5 = {
   id: 5,
   name: 'Ukraine',
   capital: 'Kiev',
   image: ukraine,
+  preview: 'https://next.odnaminyta.com/app/uploads/2020/08/image-66.jpg',
 };
 const country3 = {
   id: 3,
   name: 'Poland',
   capital: 'Warsaw',
   image: poland,
+  preview: 'https://all.accor.com/magazine/imagerie/poland_warsaw3-89bf.jpg',
 };
 
 const countries = [

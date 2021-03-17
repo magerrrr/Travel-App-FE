d27b2a95ce304b93d65ca31f7821481b617564e8
Kirill Mager <kiril.maher@itechart-group.com>
2021-03-17 20:44:34 +0300

    feat: add id for mapping ability county with param id

--- src/components/CountryCardsContainer/stubs/index.ts
+++ src/components/CountryCardsContainer/stubs/index.ts
@@ -7,19 +7,72 @@ import sweden from '../../../assets/img/resized/sweden.jpg';
 import russia from '../../../assets/img/resized/moscow.jpg';
 import ukraine from '../../../assets/img/resized/kiev.jpg';
 import poland from '../../../assets/img/resized/warsaw.jpg';
-const country1 = { name: 'Spain', capital: 'Madrid', image: spain };
-const country2 = { name: 'Belarus', capital: 'Minsk', image: minsk };
-const country8 = { name: 'France', capital: 'Paris', image: paris };
-const country4 = { name: 'Russia', capital: 'Moscow', image: russia };
-const country9 = { name: 'Italy', capital: 'Rome', image: italy };
-const country6 = { name: 'Germany', capital: 'Berlin', image: germany };
-const country7 = { name: 'Sweden', capital: 'Stockholm', image: sweden };
-const country5 = { name: 'Ukraine', capital: 'Kiev', image: ukraine };
-const country3 = { name: 'Poland', capital: 'Warsaw', image: poland };
 
-const countries = [country1, country2, country3, country4, country5, country6, country7,
-  country8, country9];
+const country1 = {
+  id: 1,
+  name: 'Spain',
+  capital: 'Madrid',
+  image: spain,
+};
+const country2 = {
+  id: 2,
+  name: 'Belarus',
+  capital: 'Minsk',
+  image: minsk,
+};
+const country8 = {
+  id: 8,
+  name: 'France',
+  capital: 'Paris',
+  image: paris,
+};
+const country4 = {
+  id: 4,
+  name: 'Russia',
+  capital: 'Moscow',
+  image: russia,
+};
+const country9 = {
+  id: 9,
+  name: 'Italy',
+  capital: 'Rome',
+  image: italy,
+};
+const country6 = {
+  id: 6,
+  name: 'Germany',
+  capital: 'Berlin',
+  image: germany,
+};
+const country7 = {
+  id: 7,
+  name: 'Sweden',
+  capital: 'Stockholm',
+  image: sweden,
+};
+const country5 = {
+  id: 5,
+  name: 'Ukraine',
+  capital: 'Kiev',
+  image: ukraine,
+};
+const country3 = {
+  id: 3,
+  name: 'Poland',
+  capital: 'Warsaw',
+  image: poland,
+};
 
-export {
-  countries
-}
+const countries = [
+  country1,
+  country2,
+  country3,
+  country4,
+  country5,
+  country6,
+  country7,
+  country8,
+  country9,
+];
+
+export { countries };

15c155803f85989a1bbeebcfa2f564f7e06c78f1
Kirill Mager <kiril.maher@itechart-group.com>
2021-03-17 20:11:10 +0300

    feat: add fetch method

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,10 +1,23 @@
 import { RouteComponentProps } from 'react-router-dom';
 import * as React from 'react';
+import CountryApiService from '../services/countryApiService';
 
 type TParams = { id: string };
 
 export const CountryPage = ({ match }: RouteComponentProps<TParams>) => {
   const { id } = match.params;
+  const api = new CountryApiService();
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const fetchedData = await api.getCountryData('belarus');
+      setData(fetchedData);
+    };
+
+    getData();
+  }, []);
 
   return (
     <h1>

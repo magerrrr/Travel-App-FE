03b8a0d432e9ae9f36d26619c00931d9dbbaebfc
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-03-19 17:57:47 +0300

    fix: correct http link, update imports

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import i18n from 'i18next';
 import { HomePage } from './pages/HomePage';
 import { CountryPage } from './pages/CountryPage';
 import { NotFoundPage } from './pages/NotFoundPage';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 import './App.scss';
 
--- src/components/Weather/fetchWeather.ts
+++ src/components/Weather/fetchWeather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL: string = 'http://api.openweathermap.org/data/2.5/weather';
+const URL: string = 'https://api.openweathermap.org/data/2.5/weather';
 const APIKEY: string = '995905e2fb21d6bf45a41cd5f589d28b';
 
 export const fetchWeather = async(query: string) => {
